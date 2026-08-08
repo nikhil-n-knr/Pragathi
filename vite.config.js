bd9c812a6549e7789cb561611f9f8b61de7023d7
Pragathi <nikhil.n@knrint.com>
2026-08-08 16:02:56 +0530

    Fix Vite optimizeDeps to ignore Ref HTML template

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
-    open: false
+    open: false,
+    watch: {
+      ignored: ['**/Ref/**']
+    }
+  },
+  optimizeDeps: {
+    entries: ['index.html']
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html')
+      }
+    }
   }
 })
